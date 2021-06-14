a6214412d22fa4318c5c5d69b6c6ccaaa2b4f6c6
Gean Rocha <Gean Rocha@Gean>
2021-06-14 19:35:59 -0300

    att

---
+++ QeepMoving.java
@@ -0,0 +1,16 @@
+package QeepMovingJava;
+
+import java.util.Scanner;
+        
+public class exercicio1 {
+        
+    public static void main(String[] args) {
+        
+                Scanner teclado = new Scanner(System.in);
+		int i = teclado.nextInt();
+		
+		System.out.printf("O numero digitado foi: %d", i);
+		
+
+	}
+    }
