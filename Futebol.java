17706cbc696e1ebe348257e0d8316b33a21adc76
Gean Rocha <Gean Rocha@Gean>
2021-06-14 19:49:04 -0300

    Get e Set

---
+++ Futebol.java
@@ -0,0 +1,19 @@
+
+public class Futebol {
+	
+	private String nomeTime;
+	public void setTime(String meuTime)
+	{
+		nomeTime = meuTime;
+	}
+
+	public String getTime()
+	{
+		return nomeTime;
+	}
+	
+	public void mostraTime()
+	{
+		System.out.printf(" Seu time do coração é %s\n", getTime());
+	}
+}
